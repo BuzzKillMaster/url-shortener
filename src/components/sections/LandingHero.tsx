2e67d60ba25a81045a14e3e9c46e5fab2357162e
BuzzKill Master <BuzzKillMaster@proton.me>
2023-02-27 09:22:56 +0100

    fix minor spelling mistake

--- src/components/sections/LandingHero.tsx
+++ src/components/sections/LandingHero.tsx
@@ -6,7 +6,7 @@ export default function LandingHero() {
             <div className={"container mx-auto lg:grid grid-cols-2 gap-32 xl:gap-64 items-center"}>
                 <div>
                     <h1 className={"text-5xl font-bold mb-8"}>Shorten your links with unparalleled ease</h1>
-                    <p className={"mb-8"}>This website is almost completely fake and only made for demonstration purposess, but the URL shortener actually does work.</p>
+                    <p className={"mb-8"}>This website is almost completely fake and only made for demonstration purposes, but the URL shortener actually does work.</p>
 
                     <ActionButton text={"Get started"}/>
                 </div>
