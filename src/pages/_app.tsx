24859a1c35b33874d969226bd376e3e5df55b276
BuzzKill Master <BuzzKillMaster@proton.me>
2023-02-27 09:17:06 +0100

    move head to app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Head from "next/head";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>URL Shortener</title>
+                <meta name="description"
+                      content="The quickest and simplest way for you to shorten your links down to a reasonable size - try it now and be amazed at the results."/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,10 @@
-import Head from 'next/head'
 import React from "react";
 import AppContainer from "@/components/AppContainer";
 
 export default function Home() {
     return (
-        <>
-            <Head>
-                <title>URL Shortener</title>
-                <meta name="description"
-                      content="The quickest and simplest way for you to shorten your links down to a reasonable size - try it now and be amazed at the results."/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main>
-                <AppContainer/>
-            </main>
-        </>
+        <main>
+            <AppContainer/>
+        </main>
     )
 }
